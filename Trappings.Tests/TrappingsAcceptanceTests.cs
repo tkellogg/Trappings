d6ce4d8e94dbe42071f52753840fa2912d2573c8
tkellogg <timothy.kellogg@gmail.com>
2012-05-29 18:57:49 -0600

    Added simplest scenario

---
+++ Trappings.Tests/TrappingsAcceptanceTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Trappings.Tests
+{
+    public class TrappingsAcceptanceTests
+    {
+        [Fact]
+        public void It_wraps_code_in_a_using_statement()
+        {
+            using (new Trappings())
+            {
+                
+            }
+        }
+    }
+}
---
+++ Trappings/Trappings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Trappings
+{
+    public sealed class Trappings : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
